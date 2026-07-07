a3dca3bae5e2bb7176e9ef2fe94b741fddc8d515
Glen <glendonnalpasan1@gmail.com>
2026-07-07 09:26:58 +0800

    Add user viewing functionality and corresponding modal

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 
     Route::livewire('/users', 'users.index')->name('users.index');
+    Route::livewire('/users/{user}', 'users.view')->name('users.show');
     Route::livewire('/contract-types', 'contract-types.index')->name('contractTypes.index');
 
 });
